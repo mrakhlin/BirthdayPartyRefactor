1cd20703ab49772f040699aeacdddfddc93d5626
Pavel Rakhlin <pavel.rakhlin@gmail.com>
2019-09-01 20:21:03 -0700

    'fix'

--- src/main/java/scrap/heap/refactor/App.java
+++ src/main/java/scrap/heap/refactor/App.java
@@ -1,38 +1,67 @@
 package scrap.heap.refactor;
 
 public class App {
+
+    public enum Color {
+    	RED,
+    	BLUE,
+    	YELLOW,
+    	BROWN
+    }
+
+	public enum Shape {
+		CIRCLE,
+		SQUARE
+	}
+
+	public enum Size {
+		LARGE,
+		MEDIUM,
+		SMALL
+	}
+
+	public enum Material {
+		MYLAR,
+		LATEX
+	}
+
+	public enum Flavor {
+		CHOCOLATE,
+		VANILLA
+	}
+
+	public enum FrostingFlavor {
+		CHOCOLATE,
+		VANILLA
+	}
+
     public String getGreeting() {
         return "Hello world.";
     }
 
     public static void main(String[] args) {
 
-         //Place birthday party orders
-         order("red", "mylar", "4", "chocolate", "chocolate", "circle", "large", "brown" );
-         order("blue", "latex", "7", "Vanilla", "chocelate", "square", "med", "brown" );
-         order("yellow", "mylar", "4", "vanilla", "vanilla", "square", "small", "yellow" );
-
+        //Place birthday party orders
+        order(Color.RED, Material.MYLAR, 4, Flavor.CHOCOLATE, FrostingFlavor.CHOCOLATE, Shape.CIRCLE, Size.LARGE, Color.BROWN);
+        order(Color.BLUE, Material.LATEX, 7, Flavor.VANILLA, FrostingFlavor.CHOCOLATE, Shape.SQUARE, Size.MEDIUM, Color.BROWN); 
+        order(Color.YELLOW, Material.MYLAR, 4, Flavor.VANILLA, FrostingFlavor.VANILLA, Shape.SQUARE, Size.SMALL, Color.YELLOW);
     }
 
-    private static void order(String balloonColor, String material, String number, String flavor, String frostingFlavor, String shape, String size, String cakeColor){
+    private static void order(Color balloonColor, Material material, int number, Flavor flavor, FrostingFlavor frostingFlavor, Shape shape, Size size, Color cakeColor){
 
         orderBalloons(balloonColor, material, number);
-
-        orderCake(frostingFlavor, flavor, shape, size, cakeColor);
+        orderCake(flavor, frostingFlavor, shape, size, cakeColor);
     }
 
-    private static void orderBalloons(String balloonColor, String material, String number){
+    private static void orderBalloons(Color balloonColor, Material material, int number){
 
         //for the purposes of this exercise, pretend this method works and adds balloons to the order
         System.out.println("Balloons ordered; " + balloonColor + ", " + material  + ", " + number);
-
     }
 
-    private static void orderCake(String flavor, String frostingFlavor, String shape, String size, String cakeColor){
+    private static void orderCake(Flavor flavor, FrostingFlavor frostingFlavor, Shape shape, Size size, Color cakeColor){
 
         //for the purposes of this exercise, pretend that this method adds a cake to the order
         System.out.println("cake ordered; " + flavor + ", " + frostingFlavor  + ", " + shape + ", " + size + ", " + cakeColor);
-
     }
-
 }
